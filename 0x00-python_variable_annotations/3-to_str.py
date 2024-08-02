13c9613c2484ca9030b3b55c9cf6d530d902cde8
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 03:58:59 +0100

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""function to_str that takes a float n
+as an argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """return the string representation of n.
+    """
+    return str(n)
