4ad66f30cf1dd00b5c1c12010db9e0bd87953a30
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 04:05:11 +0100

    sum of maxed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""type-annotated function (sum_mixed_list)
+that takes a list (mxd_list) of integers and floats as arguments and
+returns their sum as a float.
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """return the sum of a list of integers and floats.
+    """
+    max_sum = 0.0
+
+    for idx in mxd_list:
+        if isinstance(idx, int) or isinstance(idx, float):
+            max_sum += idx
+
+    return mxd_sum
