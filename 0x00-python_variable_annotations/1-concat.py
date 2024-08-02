7e97b44fe208041bc8ab8bf0090a0180826c303a
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 04:14:43 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,4 +3,6 @@
 """ Script for concantanating two strings in a function"""
 
 def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and return the result.
+    """
     return str1 + str2
