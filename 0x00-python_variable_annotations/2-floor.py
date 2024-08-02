3d7f21f56ecd7c07c12dfdb38e0b1350a349ae04
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 03:57:15 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+function that takes a float n as argument
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """return the floor of n float."""
+    return math.floor(n)
