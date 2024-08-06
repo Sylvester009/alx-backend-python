45b7665cc72ef176fa5577db07aad45ce51cb9dd
Samuel Sylvester <sasy260902@gmail.com>
2024-08-06 01:32:16 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,13 +15,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """create a list of tasks, then gather and
     sort them before returning it
     """
+    task_list = []  # init empty list to store delayed tasks
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delayed_tasks = await asyncio.as_completed(tasks)
-
-    for i in range(len(delayed_tasks)):
-        for j in range(len(delayed_tasks) - i - 1):
-            if delayed_tasks[j] > delayed_tasks[j + 1]:
-                delayed_tasks[j], delayed_Tasks[j + 1]
-                = delayed_tasks[j + 1], delayed_tasks[j]
-
-    return delayed_tasks
+    completed_tasks = asyncio.as_completed(tasks)
+    for task in completed_tasks:
+        result = await task
+        task_list.append(result)
+    return task_list
